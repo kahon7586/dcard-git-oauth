22e02da36a7ca6965bb24e4b8f2c5068dac79f25
Kahon <kahon7586@gmail.com>
2024-03-09 17:50:03 +0800

    build: for testing layout

---
+++ app/home/layout.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from "react"
+
+interface LayoutProps {
+  children: ReactNode
+}
+
+const layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <main className="absolute-center  ">
+        <div className="container flex justify-center bg-slate-400 px-4 py-2">{children}</div>
+      </main>
+    </>
+  )
+}
+
+export default layout
