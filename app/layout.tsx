f9e717545ff70d0fb6a78dc5e4444d892a3d1b51
Kahon <kahon7586@gmail.com>
2024-03-09 17:44:25 +0800

    trivial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next"
 import "./globals.css"
-import Test from "./Test"
-import AuthContextProvider from "./context/AuthContextProvider"
 
 export const metadata: Metadata = {
   title: "Create Next App",
