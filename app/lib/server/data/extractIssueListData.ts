2d0b53d4e7c285f62e55cf797dddb86e2dc9322b
Kahon <kahon7586@gmail.com>
2024-03-09 17:57:02 +0800

    build: for extracting needed data from res data

---
+++ app/lib/server/data/extractIssueListData.ts
@@ -0,0 +1,23 @@
+import { ContentData, ResData, SimpIssueData } from "@/app/ts/data/issueData"
+
+export function extractIssueListData(data: ResData[] | undefined): SimpIssueData[] | null {
+  if (data === undefined) return null
+
+  console.log(data)
+  return data.map((issue) => {
+    const { title, body, id, state, number, user } = issue
+
+    const contentData: ContentData = {
+      title,
+      body,
+      id,
+      state,
+      number,
+    }
+
+    return {
+      content: contentData,
+      user: user ? { id: user.id, login: user.login, avatar_url: user.avatar_url } : null,
+    }
+  })
+}
