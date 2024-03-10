b3f5ee08e91f4aae8458dc3a965cff8d44da130e
Kahon <kahon7586@gmail.com>
2024-03-10 18:00:14 +0800

    add: extention for div attributes

--- app/components/user/Avatar.tsx
+++ app/components/user/Avatar.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image"
-import React from "react"
+import React, { HTMLAttributes } from "react"
+import { twMerge } from "tailwind-merge"
 
-interface AvatarProps {
+interface AvatarProps extends HTMLAttributes<HTMLDivElement> {
   avatarUrl: string
   alt: string
+  className?: string
 }
-
-const Avatar = ({ avatarUrl, alt }: AvatarProps) => {
+const Avatar = ({ avatarUrl, alt, className, ...props }: AvatarProps) => {
   return (
-    <div className="size-5 relative">
+    <div
+      className={twMerge("size-5 relative", className)}
+      {...props}>
       <Image
         fill
         src={avatarUrl}
