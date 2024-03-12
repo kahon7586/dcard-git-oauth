bc78eebe8f0eda13356f800b68bcfdf8077f643f
Kahon <kahon7586@gmail.com>
2024-03-12 17:37:40 +0800

    build: convience for go backward according to current path

---
+++ app/hook/usePrevPathName.ts
@@ -0,0 +1,12 @@
+"use client"
+
+import { usePathname } from "next/navigation"
+
+const usePrevPathName = () => {
+  const pathSegment = usePathname().split("/")
+  pathSegment.pop()
+
+  return pathSegment.join("/")
+}
+
+export default usePrevPathName
