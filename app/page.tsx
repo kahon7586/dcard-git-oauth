4237f2bb62506cf5b65eea74b158177300aa09b5
Kahon <kahon7586@gmail.com>
2024-03-14 15:20:58 +0800

    change: basic layout for login

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import Link from "next/link"
-import { getCodePath } from "./lib/server/auth/getCode"
+import LoginCard from "./components/user/LoginCard"
+import { auth, signIn } from "@/auth"
+import { redirect } from "next/navigation"
 
-export default function Home() {
+export default async function page() {
+  const session = await auth()
+  let user = session?.user?.name
+
+  if (user) redirect("/home")
+
   return (
-    <div className="min-h-[100vh] flex justify-center items-center">
-      <Link href={getCodePath()}>Get code</Link>
+    <div className="min-h-[90vh] flex flex-col gap-10 justify-center items-center">
+      <LoginCard
+        role="admin"
+        action={async () => {
+          "use server"
+          await signIn("github")
+        }}
+      />
     </div>
   )
 }
