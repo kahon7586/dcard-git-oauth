5ff8d90166afaff75e2b02194224820af4d3cd87
Kahon <kahon7586@gmail.com>
2024-03-14 15:25:27 +0800

    change: get user info from auth() ,some vairable name, accessiblity for edit

--- app/home/issue/[[...postNumber]]/page.tsx
+++ app/home/issue/[[...postNumber]]/page.tsx
@@ -5,8 +5,7 @@ import { markdownParser } from "@/app/lib/server/markdown/markdownParser"
 import Link from "next/link"
 import React from "react"
 import Edit from "./pageEdit"
-
-const issueParams = ["edit"]
+import { getCurrUser } from "@/app/lib/server/auth/getCurrUser"
 
 interface PageProps {
   params: { postNumber: string[] }
@@ -17,13 +16,14 @@ const page = async ({ params }: PageProps) => {
     postNumber: [postNumber, mode],
   } = params
 
-  const { title, body, user, id, state } = await getSingleIssueData(postNumber[0])
+  const {
+    title,
+    body,
+    user: { login: author, avatar_url },
+    state,
+  } = await getSingleIssueData(postNumber[0])
 
-  if (user === null) throw Error(`User return null when loading issue: ${id}`)
-
-  if (issueParams.includes(mode)) {
-    // revalidatePath(`/home/issue/${postNumber}/edit`)
-
+  if (mode === "edit") {
     return (
       <Edit
         postNumber={postNumber}
@@ -32,7 +32,7 @@ const page = async ({ params }: PageProps) => {
     )
   }
 
-  const { login, avatar_url } = user
+  const { name: currUser, role } = await getCurrUser() // get currUser
 
   const innerHTML_sanitized = await markdownParser(body!)
   // innerHTML is sanitized by js-xss in markdownParser
@@ -45,17 +45,21 @@ const page = async ({ params }: PageProps) => {
         <Avatar
           className="flex items-baseline"
           avatarUrl={avatar_url}
-          alt={`${login} Avatar`}
+          alt={`${author} Avatar`}
         />
-        <div className="font-light text-sm text-gray-600">{login}</div>
+        <div className="font-light text-sm text-gray-600">{author}</div>
       </div>
 
       <div /*body*/ className="relative px-6 py-4 border rounded-lg">
-        <Link
-          className="absolute hover:bg-slate-200 border rounded-md px-2 py-1 right-3 top-2"
-          href={`/home/issue/${postNumber}/edit`}>
-          Edit
-        </Link>
+        {currUser && currUser === author && role === "admin" ? (
+          // currently there's no edit permission for admin
+          <Link
+            className="absolute hover:bg-slate-200 border rounded-md px-2 py-1 right-3 top-2"
+            href={`/home/issue/${postNumber}/edit`}>
+            Edit
+          </Link>
+        ) : null}
+
         <IssueBody sanitized_innerHTML={innerHTML_sanitized} />
       </div>
     </div>
