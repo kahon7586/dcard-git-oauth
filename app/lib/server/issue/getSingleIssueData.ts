f7f48beea7b265e06fac96024dbdbeee8efd19df
Kahon <kahon7586@gmail.com>
2024-03-14 18:24:36 +0800

    fix2: the problem that edit with wrong post

--- app/lib/server/issue/getSingleIssueData.ts
+++ app/lib/server/issue/getSingleIssueData.ts
@@ -8,7 +8,7 @@ export async function getSingleIssueData(postID: string | number) {
   const res = await octokit.request("GET /repos/{owner}/{repo}/issues/{issue_number}", {
     owner: process.env.OWNER!,
     repo: process.env.REPO!,
-    issue_number: 2,
+    issue_number: postID,
     headers: {
       "X-GitHub-Api-Version": "2022-11-28",
     },
