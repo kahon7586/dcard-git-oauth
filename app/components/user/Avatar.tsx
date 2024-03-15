d4a9fcf3d15becca9c44586cd3e5fe39f5eabb97
Kahon <kahon7586@gmail.com>
2024-03-15 18:05:31 +0800

    add: size prop for performance

--- app/components/user/Avatar.tsx
+++ app/components/user/Avatar.tsx
@@ -14,6 +14,7 @@ const Avatar = ({ avatarUrl, alt, className, ...props }: AvatarProps) => {
       {...props}>
       <Image
         fill
+        sizes="width: 1.5rem height:1.5rem"
         src={avatarUrl}
         alt={alt}
       />
