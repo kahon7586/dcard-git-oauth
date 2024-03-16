632e7db4238de87f5f7cb78bd0ff28b492df4d54
Kahon <kahon7586@gmail.com>
2024-03-16 15:52:45 +0800

    fix: the problem with re-render when no new data; add: some loading status

--- app/components/IssueList.tsx
+++ app/components/IssueList.tsx
@@ -1,46 +1,54 @@
 "use client"
 
-import React, { ReactNode, useEffect, useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { useInfiniteScroll } from "../hook/useInfiniteScroll"
+import Spinner from "./Spinner"
 
 interface IssueListProps {
-  issueNodeList: ReactNode[] | null
+  firstPageData: React.JSX.Element[] | null
   action: (pages: number) => Promise<React.JSX.Element[] | null>
 }
 
-const IssueList = ({ issueNodeList, action: getMoreData }: IssueListProps) => {
+const IssueList = ({ firstPageData, action: getIssueNodeList }: IssueListProps) => {
   const issueListRef = useRef<HTMLDivElement | null>(null)
 
-  const [page, setPage] = useState(2)
-  const [list, setList] = useState(issueNodeList ?? [])
+  const pageRef = useRef(2)
+  const [list, setList] = useState(firstPageData)
+  const [isNoMoreData, setIsNoMoreData] = useState(firstPageData === null)
 
   const [isBottom, setIsBottom] = useInfiniteScroll(issueListRef)
 
   useEffect(() => {
-    if (isBottom === false) return
+    if (isBottom === false || isNoMoreData === true) return
 
     async function pushData() {
-      const newNodeList = await getMoreData(page)
+      const newNodeList = await getIssueNodeList(pageRef.current)
 
-      console.log(`newNodeList: ${newNodeList}`)
-
-      if (newNodeList === null) return
-
-      setList((prev) => [...prev, ...newNodeList])
-      setPage((prev) => prev + 1) // fix for no new data but still increase
+      if (newNodeList === null) {
+        setIsNoMoreData(true)
+        return
+      }
+      setList((prev) => [...prev!, ...newNodeList])
+      pageRef.current += 1
       setIsBottom(false)
     }
 
     pushData()
   }, [isBottom])
 
-  if (issueNodeList === null) return <div> no issue data! </div>
-
   return (
     <div
-      className="container flex flex-col bg-slate-300 max-h-[300px] overflow-auto"
+      className="container flex flex-col  bg-slate-300 max-h-[300px] overflow-auto"
       ref={issueListRef}>
       {list}
+      {isNoMoreData ? (
+        <div className="flex justify-center my-4">No More Data!</div>
+      ) : (
+        <div className="flex justify-center items-center my-4">
+          <Spinner />
+          Loading...
+        </div>
+      )}
     </div>
   )
 }
