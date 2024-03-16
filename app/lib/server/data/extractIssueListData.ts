ab94741daaaf66d2f0ffa520d13e828a3f87b961
Kahon <kahon7586@gmail.com>
2024-03-16 15:50:00 +0800

    fix: wrong input type

--- app/lib/server/data/extractIssueListData.ts
+++ app/lib/server/data/extractIssueListData.ts
@@ -1,9 +1,8 @@
 import { ContentData, ResData, SimpIssueData } from "@/app/ts/data/issueData"
 
-export function extractIssueListData(data: ResData[] | undefined): SimpIssueData[] | null {
-  if (data === undefined) return null
+export function extractIssueListData(data: ResData[]) {
+  if (data.length === 0) return null
 
-  // console.log(data)
   return data.map((issue) => {
     const { title, body, id, state, number, user } = issue
 
@@ -15,9 +14,11 @@ export function extractIssueListData(data: ResData[] | undefined): SimpIssueData
       number,
     }
 
-    return {
+    const extractedData: SimpIssueData = {
       content: contentData,
       user: user ? { id: user.id, login: user.login, avatar_url: user.avatar_url } : null,
     }
+
+    return extractedData
   })
 }
