0ecc776e65701b78986c120c554de7fbc1e86d9b
Kahon <kahon7586@gmail.com>
2024-03-19 15:26:19 +0800

    change: rename variable to avoid misleading, min-w

--- app/components/client/IssueBody.tsx
+++ app/components/client/IssueBody.tsx
@@ -5,14 +5,14 @@ import "github-markdown-css"
 import "./IssueBody.css"
 
 interface IssueBodyProps {
-  sanitized_innerHTML: string
+  innerHTML: string | undefined
 }
 
-const IssueBody = ({ sanitized_innerHTML }: IssueBodyProps) => {
+const IssueBody = ({ innerHTML }: IssueBodyProps) => {
   return (
     <div
-      className="markdown-body px-4 py-2  border rounded-lg"
-      dangerouslySetInnerHTML={{ __html: sanitized_innerHTML }}></div>
+      className="markdown-body px-4 py-2 border rounded-lg min-h-[3rem]"
+      dangerouslySetInnerHTML={{ __html: innerHTML ?? "No description provided." }}></div>
   )
 }
 
