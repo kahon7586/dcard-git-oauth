41ed112c6a8e3b881e56f94601503804b3946396
Kahon <kahon7586@gmail.com>
2024-03-19 15:27:23 +0800

    build: delete btn action

---
+++ app/lib/server/issue/closeIssue.ts
@@ -0,0 +1,29 @@
+"use server"
+
+import { redirect } from "next/navigation"
+import { getOctokit } from "../auth/getOctokit"
+import { getStatusMessage } from "../github/getStatusMessage"
+import { revalidatePath } from "next/cache"
+
+export async function closeIssue(postNumber: number) {
+  const octokit = await getOctokit()
+
+  const res = await octokit.request("PATCH /repos/{owner}/{repo}/issues/{issue_number}", {
+    owner: process.env.OWNER!,
+    repo: process.env.REPO!,
+    issue_number: postNumber,
+    state: "closed",
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  })
+
+  const { status } = res as { data: unknown; status: number }
+
+  if (status === 200) {
+    revalidatePath("/issue-list")
+    redirect("/issue-list")
+  }
+
+  getStatusMessage(status, closeIssue.name)
+}
