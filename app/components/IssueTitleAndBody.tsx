a68adb093924a12f6ff36c1fdd3510ca820622a8
Kahon <kahon7586@gmail.com>
2024-03-19 15:25:32 +0800

    build: code splitting

---
+++ app/components/IssueTitleAndBody.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link"
+import React from "react"
+import { closeIssue } from "../lib/server/issue/closeIssue"
+import { getSingleIssueData } from "../lib/server/issue/getSingleIssueData"
+import DeleteBtn from "./DeleteBtn"
+import IssueBody from "./client/IssueBody"
+import AdminOnly from "./user/AdminOnly"
+import Avatar from "./user/Avatar"
+
+interface IssueTitleAndBodyProps {
+  postNumber: number
+}
+
+const IssueTitleAndBody = async ({ postNumber }: IssueTitleAndBodyProps) => {
+  const {
+    title,
+    body_html,
+    user: { login: author, avatar_url },
+    state,
+  } = await getSingleIssueData(postNumber)
+
+  // ! Potential XSS attack may exist in body
+
+  return (
+    <>
+      <div /*title*/ className="px-2 py-1 flex gap-2 items-baseline border rounded-lg">
+        <div className="font-bold text-2xl">{title}</div>
+        <div className="text-slate-600 before:content-['('] after:content-[')']">{state}</div>
+        <Avatar
+          className="flex items-baseline"
+          avatarUrl={avatar_url}
+          alt={`${author} Avatar`}
+        />
+        <div className="font-light text-sm text-gray-600">{author}</div>
+      </div>
+
+      <div /*body*/ className="relative">
+        <AdminOnly>
+          <div className="absolute flex right-3 top-2 gap-2">
+            <Link
+              className="bg-slate-300 hover:bg-slate-200 border rounded-md px-2 py-1 "
+              href={`/issue-list/edit/${postNumber}`}>
+              Edit
+            </Link>
+            <DeleteBtn
+              className="bg-slate-300 hover:bg-slate-200 border rounded-md px-2 py-1 "
+              action={closeIssue}
+              postNumber={postNumber}
+            />
+          </div>
+        </AdminOnly>
+
+        <IssueBody innerHTML={body_html} />
+      </div>
+    </>
+  )
+}
+
+export default IssueTitleAndBody
--- app/issue-list/issue/[postNumber]/page.tsx
+++ app/issue-list/issue/[postNumber]/page.tsx
@@ -1,50 +1,16 @@
-import IssueBody from "@/app/components/client/IssueBody"
-import Avatar from "@/app/components/user/Avatar"
-import { getSingleIssueData } from "@/app/lib/server/issue/getSingleIssueData"
-import Link from "next/link"
 import React from "react"
-import AdminOnly from "@/app/components/user/AdminOnly"
+import IssueTitleAndBody from "@/app/components/IssueTitleAndBody"
 
 interface PageProps {
   params: { postNumber: string }
 }
 
 const page = async ({ params }: PageProps) => {
-  const postNumber = params.postNumber
-
-  const {
-    title,
-    body_html,
-    user: { login: author, avatar_url },
-    state,
-  } = await getSingleIssueData(postNumber)
-
-  // ! Potential XSS attack may exist in body
+  const postNumber = Number(params.postNumber)
 
   return (
     <div className="flex flex-col w-full px-4 py-2 gap-4">
-      <div /*title*/ className="px-2 py-1 flex gap-2 items-baseline border rounded-lg">
-        <div className="font-bold text-2xl">{title}</div>
-        <div className="text-slate-600 before:content-['('] after:content-[')']">{state}</div>
-        <Avatar
-          className="flex items-baseline"
-          avatarUrl={avatar_url}
-          alt={`${author} Avatar`}
-        />
-        <div className="font-light text-sm text-gray-600">{author}</div>
-      </div>
-
-      <div /*body*/ className="relative">
-        <AdminOnly>
-          <Link
-            className="absolute bg-slate-300 hover:bg-slate-200 border rounded-md px-2 py-1 right-3 top-2"
-            href={`/issue-list/edit/${postNumber}`}>
-            Edit
-          </Link>
-        </AdminOnly>
-
-        <IssueBody sanitized_innerHTML={body_html!} />
-      </div>
+      <IssueTitleAndBody postNumber={postNumber} />
     </div>
   )
 }
