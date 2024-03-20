d787fd3de956ff6726e7e9dad91c254bb6a07a2b
Kahon <kahon7586@gmail.com>
2024-03-20 15:42:04 +0800

    change: combine github-markdown-css into global as well as the style of markdown-body

--- app/components/client/IssueBody.tsx
+++ app/components/client/IssueBody.tsx
@@ -1,8 +1,6 @@
 "use client"
 
 import React from "react"
-import "github-markdown-css"
-import "./IssueBody.css"
 
 interface IssueBodyProps {
   innerHTML: string | undefined
@@ -11,7 +9,7 @@ interface IssueBodyProps {
 const IssueBody = ({ innerHTML }: IssueBodyProps) => {
   return (
     <div
-      className="markdown-body px-4 py-2 border rounded-lg min-h-[3rem]"
+      className="markdown-body"
       dangerouslySetInnerHTML={{ __html: innerHTML ?? "No description provided." }}></div>
   )
 }
