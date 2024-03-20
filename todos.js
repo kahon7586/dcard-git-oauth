770f44b217603165ee9681a196044ad1625e5ef5
Kahon <kahon7586@gmail.com>
2024-03-20 16:13:17 +0800

    DONE: markdown preview

--- todos.js
+++ todos.js
@@ -3,9 +3,9 @@ WIP: try to build a filter in list, see:
 https://medium.com/%E6%89%8B%E5%AF%AB%E7%AD%86%E8%A8%98/react-hooks-usestate-vs-usereducer-b14966ad37dd
 https://overreacted.io/a-complete-guide-to-useeffect/
 
-//WIP: length test for title and body
+// WIP: length test for title and body
 
-WIP: markdown preview
+// WIP: markdown preview
 
 WIP: deal with switch status code
 */
