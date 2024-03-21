890c4e93f28e404ad672919c5ff3de9622f2bc9b
Kahon <kahon7586@gmail.com>
2024-03-21 15:36:54 +0800

    remove: dark mode theme

--- app/components/user/LoginCard.tsx
+++ app/components/user/LoginCard.tsx
@@ -16,13 +16,13 @@ const ROLE_INFO = {
 const LoginCard = ({ role, action }: LoginCard) => {
   return (
     <form
-      className="min-w-[400px] p-6 flex justify-center flex-col items-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+      className="min-w-[400px] p-6 flex justify-center flex-col items-center bg-white border border-gray-200 rounded-lg shadow"
       action={action}>
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{role}</h5>
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-slate-600">{role}</h5>
 
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{ROLE_INFO[role]}</p>
       <button
-        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
         type="submit">
         Continue
         <svg
