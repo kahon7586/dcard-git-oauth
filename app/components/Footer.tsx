d70d3e171ff635e0b31e68fe7a1e74583aea7273
Kahon <kahon7586@gmail.com>
2024-03-29 12:31:52 +0800

    change: code spiltting for readibility

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,42 +1,50 @@
-import { auth } from "@/auth";
-import { signOut } from "next-auth/react";
+import { auth, signOut } from "@/auth";
 import React from "react";
 import Button from "./Button";
 import LinkButton from "./LinkButton";
+import { getRepository } from "../lib/server/github/getRepository";
 
-const Footer = async () => {
+const UserInfo = async ({}) => {
   const session = await auth();
   const userName = session?.user?.name;
   const userRole = session?.user?.role;
 
+  async function signOutAction() {
+    "use server";
+    await signOut();
+  }
+
   return (
-    <footer className="flex-grow-1 flex items-center justify-center gap-4 py-4 [&:nth-child(2)]:flex-grow">
-      {session ? (
-        <form
-          action={async () => {
-            "use server";
-            await signOut();
-          }}
-        >
-          <div className="flex flex-col items-center gap-2">
-            {`Name: ${userName}, Role: ${userRole}`}
-            <div>
-              <Button className="rounded-md border px-2 py-1" type="submit">
-                Sign Out
-              </Button>
-            </div>
-          </div>
-        </form>
-      ) : (
-        <div className="flex flex-col items-center gap-2">
-          {'Name: Anonymous, Role: "user"'}
-          <div>
+    <form action={signOutAction}>
+      <div className="flex flex-col items-center gap-2">
+        {`Name: ${userName ?? "Anonymous"}, Role: ${userRole ?? "user"}`}
+        <div>
+          {session ? (
+            <Button className="rounded-md border px-2 py-1" type="submit">
+              Sign Out
+            </Button>
+          ) : (
             <LinkButton className="rounded-md border px-2 py-1" href="/">
               Login
             </LinkButton>
-          </div>
+          )}
         </div>
-      )}
+      </div>
+    </form>
+  );
+};
+
+const Footer = async () => {
+  const currRepo = await getRepository();
+
+  console.log(currRepo);
+
+  return (
+    <footer className="flex-grow-1 flex flex-col items-center justify-center gap-4 py-4 [&:nth-child(2)]:flex-grow">
+      <UserInfo />
+      {currRepo
+        ? `Repo: ${currRepo.repo}, Owner: ${currRepo.owner}`
+        : "Repository not specified!"}
     </footer>
   );
 };
