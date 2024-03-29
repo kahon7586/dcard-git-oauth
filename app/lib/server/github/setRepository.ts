0ceee20ffcfd1f956fe2a20c44ea14c831b99c0b
Kahon <kahon7586@gmail.com>
2024-03-29 17:39:00 +0800

    trivial

--- app/lib/server/github/setRepository.ts
+++ app/lib/server/github/setRepository.ts
@@ -1,6 +1,5 @@
 import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
 export async function setRepository(formData: FormData) {
   "use server";
