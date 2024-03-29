1365f3091d3c47184718aa4585530ac2f567e79d
Kahon <kahon7586@gmail.com>
2024-03-29 16:40:14 +0800

    add: repo set link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -16,6 +16,7 @@ const Header = () => {
     <header className="flex items-center justify-between gap-6 px-6 py-2 text-xl font-bold shadow-md shadow-primary-hover dark:shadow-md dark:shadow-primary-hover-d">
       <div className="flex gap-6">
         <Link href="/">Home</Link>
+        <Link href="/set-repository">Repo</Link>
         <Link href="/issue-list">Issue List</Link>
         <Link href="/test-loading">Loading</Link>
         <Link href="/test-error">Error</Link>
