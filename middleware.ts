0646d6dfbb6fe7075e42fd880296242d18d3aad5
Kahon <kahon7586@gmail.com>
2024-03-30 16:20:04 +0800

    add: redirect user when is not admin but access to admin only route

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "./auth";
+
 export { auth as default } from "./auth";
+
+const ADMIN_ONLY_ROUTES = ["/edit", "/new-post"];
+
+function isAccessAdminRoutes(pathname: string) {
+  return ADMIN_ONLY_ROUTES.some((segment) => {
+    return pathname.includes("/issue-list" + segment);
+  });
+}
+
+export async function middleware(req: NextRequest, res: NextResponse) {
+  const session = await auth();
+  const userRole = session?.user?.role;
+
+  if (userRole === undefined || userRole === "user") {
+    if (isAccessAdminRoutes(req.nextUrl.pathname)) {
+      return NextResponse.redirect(new URL("/issue-list", req.url));
+    }
+  }
+
+  return NextResponse.next();
+}
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
