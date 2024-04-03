48213b4e73d0977b8aee9eafc31e9ce9a85c5e8f
Kahon <kahon7586@gmail.com>
2024-04-03 16:43:34 +0800

    fix: show a better error message due to nextjs only show generic error message in production mode

--- app/error.tsx
+++ app/error.tsx
@@ -1,4 +1,8 @@
-"use client"; // Error components must be Client Components
+"use client";
+import { useEffect } from "react";
+
+// Error components must be Client Components
+// Nextjs only show generic error message in production mode.
 
 export default function Error({
   error,
@@ -10,21 +14,28 @@ export default function Error({
 }) {
   const { message, digest } = error;
 
+  useEffect(() => {
+    console.log(message);
+  });
+
   return (
     <div className="absolute-center flex-col  text-center">
       <div className="space-y-1">
         <h1 className="text-[6rem]">Oops...</h1>
         <h2 className="text-[2.5rem]">Something went wrong!</h2>
         <div className="space-y-2">
-          <p>{"Error message: " + message}</p>
-          <p>{"Digest: " + digest}</p>
+          <p>
+            {"If this error keep showing, contact us with this digest code: " +
+              digest}
+          </p>
         </div>
       </div>
       <div className="my-8 space-y-2">
-        <p className="text-[1.3rem]">
-          <span className="text-[2rem]">🦈</span>: But don't worry!
-          <span className="italic"> Mr.Shark is here!</span>
+        <p className="text-[1.3rem]">Before we deal with it... </p>
+        <p className="my-2 text-[2rem]">
+          🦈<span className="italic">: Mr.Shark is here!</span>
         </p>
+
         <p>He will be with you until we fix the problem!</p>
       </div>
     </div>
