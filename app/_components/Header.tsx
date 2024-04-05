aa1131963bc9efdee8bc7899f9de36209c927e37
Kahon <kahon7586@gmail.com>
2024-04-05 13:37:52 +0800

    change: split nav link into components; add: RWD for mobile

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,27 +1,12 @@
 import Link from "next/link";
 import React from "react";
 import ThemeToggler from "./client/ThemeToggler";
+import HamburgerMenu from "./client/HamburgerMenu";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between gap-6 px-6 py-2 text-xl font-bold shadow-md shadow-primary-hover dark:shadow-md dark:shadow-primary-hover-d">
-      <div className="block gap-6 md:flex">
-        <Link className="block" href="/">
-          Home
-        </Link>
-        <Link className="block" href="/set-repository">
-          Repo
-        </Link>
-        <Link className="block" href="/issue-list">
-          Issue List
-        </Link>
-        <Link className="block" href="/test-loading">
-          Loading
-        </Link>
-        <Link className="block" href="/test-error">
-          Error
-        </Link>
-      </div>
+    <header className="flex items-start justify-between gap-6 px-6 py-2 text-xl font-bold shadow-md shadow-primary-hover dark:shadow-md dark:shadow-primary-hover-d">
+      <HamburgerMenu />
 
       <ThemeToggler />
     </header>
--- app/_components/client/HamburgerMenu.tsx
+++ app/_components/client/HamburgerMenu.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+"use client";
+
+import Link from "next/link";
+import React, { useState } from "react";
+import Button from "../Button";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const HamburgerMenu = () => {
-  return <div></div>;
+  const [isToggle, setIsToggle] = useState(false);
+
+  function handleClickMenu() {
+    setIsToggle((prev) => !prev);
+  }
+
+  return (
+    <div onClick={handleClickMenu}>
+      <Button className="border-none  md:hidden">
+        <GiHamburgerMenu />
+      </Button>
+      <div
+        className={`${isToggle ? "block space-y-1 px-2" : "hidden"} gap-6 md:flex`}
+      >
+        <Link className="block" href="/">
+          Home
+        </Link>
+        <Link className="block" href="/set-repository">
+          Repo
+        </Link>
+        <Link className="block" href="/issue-list">
+          Issue List
+        </Link>
+        <Link className="block" href="/test-loading">
+          Loading
+        </Link>
+        <Link className="block" href="/test-error">
+          Error
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default HamburgerMenu;
