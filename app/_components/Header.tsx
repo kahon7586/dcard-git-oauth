d23a08a347956ddbe3c9e718d369b5db43f5e75a
Kahon <kahon7586@gmail.com>
2024-04-05 12:30:08 +0800

    WIP: header design for phone

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,4 +1,3 @@
-import dynamic from "next/dynamic";
 import Link from "next/link";
 import React from "react";
 import ThemeToggler from "./client/ThemeToggler";
@@ -6,12 +5,22 @@ import ThemeToggler from "./client/ThemeToggler";
 const Header = () => {
   return (
     <header className="flex items-center justify-between gap-6 px-6 py-2 text-xl font-bold shadow-md shadow-primary-hover dark:shadow-md dark:shadow-primary-hover-d">
-      <div className="flex gap-6">
-        <Link href="/">Home</Link>
-        <Link href="/set-repository">Repo</Link>
-        <Link href="/issue-list">Issue List</Link>
-        <Link href="/test-loading">Loading</Link>
-        <Link href="/test-error">Error</Link>
+      <div className="block gap-6 md:flex">
+        <Link className="block" href="/">
+          Home
+        </Link>
+        <Link className="block" href="/set-repository">
+          Repo
+        </Link>
+        <Link className="block" href="/issue-list">
+          Issue List
+        </Link>
+        <Link className="block" href="/test-loading">
+          Loading
+        </Link>
+        <Link className="block" href="/test-error">
+          Error
+        </Link>
       </div>
 
       <ThemeToggler />
