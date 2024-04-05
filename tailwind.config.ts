12626cea9585aa5fa9d760901712d0b7345fd52d
Kahon <kahon7586@gmail.com>
2024-04-05 13:36:25 +0800

    fix: stick hover

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   darkMode: "selector",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
